8c983c2af1e150009e994862fa1641fa1c88556e
kasbitch <44318531+kasbitch@users.noreply.github.com>
2020-06-03 14:59:13 +0700

    Update request.js

--- src/store/request.js
+++ src/store/request.js
@@ -44,7 +44,7 @@ export default {
           const fileLink = document.createElement('a');
 
           fileLink.href = fileURL;
-          fileLink.setAttribute('download', 'file.pdf');
+          fileLink.setAttribute('download', 'newFile.csv');
           document.body.appendChild(fileLink);
 
           fileLink.click();
