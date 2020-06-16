c9a69f2349b1d3903d5716297f163a3cbb25c633
anatoly <tolyan011998@gmail.com>
2020-06-16 17:21:57 +0700

    commit Tolya 16.06.2020

---
+++ dist/precache-manifest.d304b17591ae018080679c6ecb085008.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4dd630a8f93d9432e1",
+    "url": "/css/about.1f15cdb8.css"
+  },
+  {
+    "revision": "1815f413414966f881c3",
+    "url": "/css/app.0aa8e7f7.css"
+  },
+  {
+    "revision": "053bf782d423ff4cd3c3",
+    "url": "/css/chunk-vendors.6229d9ed.css"
+  },
+  {
+    "revision": "595b54bc9be0a9ed6f165e1aca2130c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4dd630a8f93d9432e1",
+    "url": "/js/about.c16e0382.js"
+  },
+  {
+    "revision": "1815f413414966f881c3",
+    "url": "/js/app.d5a5229d.js"
+  },
+  {
+    "revision": "053bf782d423ff4cd3c3",
+    "url": "/js/chunk-vendors.8a9bee12.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
